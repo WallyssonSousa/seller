e78c6240f8572f380ebc6e2344c3a3630b461303
NicolasDiasReis <nico.reisdias@gmail.com>
2025-08-21 09:00:31 -0300

    Testando com SQLite

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from src.routes import auth_bp
-from src.Config.data_base import init_db
+from src.Config.data_base import init_db, db
+from src.Infrastructure.Model.user import User
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +14,18 @@ def create_app():
 
     return app
 
+def create_tables(app):
+    """
+    Cria as tabelas do banco automaticamente com SQLAlchemy.
+    """
+    with app.app_context():
+        db.create_all()  # Cria todas as tabelas definidas nos Models
+
 if __name__ == '__main__':
     app = create_app()
+    
+    # Cria as tabelas SQLite
+    create_tables(app)
+    
+    # Roda a API
     app.run(host="0.0.0.0", port=5000, debug=True)
--- src/Config/data_base.py
+++ src/Config/data_base.py
@@ -1,11 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
 
+# Inicializa o SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
     """
-    Inicializa a base de dados com o app Flask e o SQLAlchemy.
+    Inicializa a base de dados com o app Flask e o SQLAlchemy usando SQLite.
     """
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:1234@localhost:5432/market_management'
+
+    # SQLite: o banco será criado no arquivo local 'market_management.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market_management.db'
+    
+    # Desativa notificações de alteração de objeto (melhora performance)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Inicializa o SQLAlchemy com o app Flask
     db.init_app(app)
