07206653a9b21ebcb5face1844478b918369f659
Wallysson <118409802+WallyssonSousa@users.noreply.github.com>
2025-09-01 20:27:19 -0300

    Feat: Comentando a class que envia o código para o Whats

--- src/Infrastructure/http/whats_app.py
+++ src/Infrastructure/http/whats_app.py
@@ -1,5 +1,6 @@
 # Wallysson - classe para gerenciar o envio de mensagem 
-from twilio.rest import Client
+""" Envia o código de verificacao pelo WhatsApp """
+""" from twilio.rest import Client
 
 class WhatsAppService:
     def __init__(self, conta_sid: str, auth_token: str, from_numero: str):
@@ -7,10 +8,10 @@ class WhatsAppService:
         self.from_numero = f"whatsapp:{from_numero}"
 
     def enviar_mensagem(self, to_numero: str, codigo: str):
-        """ Envia o código de verificacao pelo WhatsApp """
         message = self.cliente.messages.create(
             from_=self.from_numero,
             body=f"Seu código de verificação é: {codigo}",
             to=f"whatsapp:{to_numero}"
         )
         return message.sid   
+ """
