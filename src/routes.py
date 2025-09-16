70439cf475d84343b633d099d261466a86221a79
NicolasDiasReis <nico.reisdias@gmail.com>
2025-09-16 18:17:14 -0300

    Testando modificação no GET (health check)

--- src/routes.py
+++ src/routes.py
@@ -1,17 +1,14 @@
 from flask import Blueprint, jsonify, make_response
 from src.Application.Controller.user_controller import UserController
 
-# Criando o Blueprint
 auth_bp = Blueprint("auth", __name__)
 
-# Health check
 @auth_bp.route('/api', methods=['GET'])
 def health():
     return make_response(jsonify({
-        "mensagem": "API - OK; Docker - Up",
+        "mensagem": "API - OK"
     }), 200)
 
-# Cadastro de usuário
 @auth_bp.route('/users', methods=['POST'])
 def register_user():
     return UserController.register_user()
