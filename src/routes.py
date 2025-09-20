9abc63d6d63ad4aca3e2ba91e5f048c883bea256
Pedrolopezz87 <101686488+Pedrolopezz87@users.noreply.github.com>
2025-09-20 01:01:57 -0300

    Corrige AuthController para métodos de instância e login/cadastro

--- src/routes.py
+++ src/routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from src.Application.Controller.auth_controller import AuthController
 from src.Application.Controller.product_controller import ProductController
+from src.Application.Controller.user_controller import UserController
+from flask import make_response, request
 
 auth_bp = Blueprint("auth", __name__)
 product_bp = Blueprint("product", __name__)
@@ -21,11 +23,11 @@ def verificar_codigo():
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
-    return jsonify(auth_controller.login())
+    return jsonify(AuthController.login())
 
 @auth_bp.route('/cadastro', methods=['GET'])
 def cadastro():
-    return jsonify(auth_controller.cadastro())
+    return jsonify(AuthController.cadastro())
 
 @product_bp.route('/products', methods=['GET'])
 def listar_produtos():
@@ -33,4 +35,4 @@ def listar_produtos():
 
 @auth_bp.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    return User_controller.get_user_by_id(user_id)
+    return UserController.get_user_by_id(user_id)
