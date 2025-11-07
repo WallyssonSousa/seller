3794fa5aba315319680fd8bf3610712b5a2d6fbb
Wallysson <118409802+WallyssonSousa@users.noreply.github.com>
2025-11-06 21:56:31 -0300

    fix: correcting IPv4 error

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
+# força IPv4
 import socket
-
-orig = socket.getaddrinfo
-def force_ipv4(host, port, family=0, type=0, proto=0, flags=0):
-    return orig(host, port, socket.AF_INET, type, proto, flags)
+def force_ipv4(host, port, *args, **kwargs):
+    return socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
 socket.getaddrinfo = force_ipv4
 
-
 from flask import Flask
-from src.routes import auth_bp, product_bp
+from src.routes import auth_bp, product_bp, sale_bp
 from src.Config.data_base import init_db, db
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
-from src.routes import sale_bp
 from flask_cors import CORS
 import os
 
@@ -19,6 +16,7 @@ load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
     CORS(
         app,
         resources={r"/*": {"origins": "*"}},
@@ -29,30 +27,22 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY", "change_me")
-    app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY", "change_me_jwt")
+    app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
+    app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 
     init_db(app)
-
-    jwt = JWTManager(app)
+    JWTManager(app)
 
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(product_bp, url_prefix='/product')
     app.register_blueprint(sale_bp, url_prefix='/sale')
 
-    return app
-
-
-def create_tables(app):
-    with app.app_context():
-        db.create_all()
-
-
-if __name__ == '__main__':
-    app = create_app()
-
     @app.route("/")
     def health():
         return {"status": "ok"}, 200
 
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True)
