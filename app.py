b37c64677521a00e5f337b5dc1b40fc180cb85ef
Wallysson <118409802+WallyssonSousa@users.noreply.github.com>
2025-11-10 20:19:35 -0300

    chore: removing the socket in app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# força IPv4
-import socket
-def force_ipv4(host, port, *args, **kwargs):
-    return socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
-socket.getaddrinfo = force_ipv4
-
 from flask import Flask
 from src.routes import auth_bp, product_bp, sale_bp
 from src.Config.data_base import init_db, db
@@ -17,6 +11,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
+    # CORS
     CORS(
         app,
         resources={r"/*": {"origins": "*"}},
@@ -25,6 +20,7 @@ def create_app():
         methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
     )
 
+    # Configurações
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
@@ -43,6 +39,14 @@ def create_app():
 
     return app
 
+
+def create_tables(app):
+    with app.app_context():
+        db.create_all()
+        print("✅ Tabelas criadas com sucesso!")
+
+
 if __name__ == '__main__':
     app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    create_tables(app)
+    app.run(host="127.0.0.1", port=5000, debug=True)
