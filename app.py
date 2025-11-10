2abfebb1c6534e5a72cdfd8beabf740b63c52815
Wallysson <118409802+WallyssonSousa@users.noreply.github.com>
2025-11-10 20:33:09 -0300

    fix: correcting server host in app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def create_tables(app):
 if __name__ == '__main__':
     app = create_app()
     create_tables(app)
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
